b7dcb5ab00b37d2ab1936b1ca2c4e5128a0273d4
Venugopal-VL <venugopal.kallam@vassardigital.ai>
2026-06-05 10:20:12 +0530

    feat: connect PostgreSQL, run migrations, register Job in admin

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ll)*_08ft_ot!3md8tcv^(#cpapuj#jpf*$e-x(qe2z7=s^wcm'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'jobs'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +79,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Job
 
 # Register your models here.
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['id', 'task_type', 'status', 'priority', 'created_at', 'retry_count']
+    list_filter = ['status', 'priority']
+    search_fields = ['task_type']
